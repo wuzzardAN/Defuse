9fbea08434b2875f516677fb64f9b9cbdfa56a04
wuzzardAN <eqemen.emre0880@gmail.com>
2021-04-02 23:40:31 +0300

    fix bug

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -35,7 +35,8 @@ public class LevelManager : MonoBehaviour
         levelCount++;
         PlayerPrefs.SetInt("Levels", levelCount);
         PlayerPrefs.Save();
-        restartLevelPanel.SetActive(true);
+        finishGamePanel.SetActive(true);
+        Time.timeScale = 0;
       }
     }
     //Timer
@@ -86,13 +87,13 @@ public class LevelManager : MonoBehaviour
       }
       if(levelCount == 1) {
         timeRemaining = 70;
-        minigameFull = 2;
+        minigameFull = 1;
         levelText.text = "Level " + levelCount.ToString();
         FindObjectOfType<MiniGameShow5>().wireTaskShow();
     }
       if(levelCount == 2){
         timeRemaining = 80;
-        minigameFull = 3;
+        minigameFull = 2;
         levelText.text = "Level " + levelCount.ToString();
         FindObjectOfType<MiniGameShow2>().wireTaskShow();
         FindObjectOfType<MiniGameShow6>().simonSaysShow();
@@ -115,14 +116,14 @@ public class LevelManager : MonoBehaviour
       }
       if(levelCount == 5){
         timeRemaining = 91;
-        minigameFull = 3;
+        minigameFull = 2;
         levelText.text = "Level " + levelCount.ToString();
         FindObjectOfType<MiniGameShow3>().simonSaysShow();
         FindObjectOfType<MiniGameShow4>().numFindShow();
       }
       if(levelCount == 6){
         timeRemaining = 76;
-        minigameFull = 3;
+        minigameFull = 2;
         levelText.text = "Level " + levelCount.ToString();
         FindObjectOfType<MiniGameShow7>().wireTaskShow();
         FindObjectOfType<MiniGameShow1>().numFindShow();
